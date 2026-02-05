59749153032f624a3c1a9c7fd82cbcace3f23e14
dazavv <dasha7348@gmail.com>
2026-02-05 17:08:49 +0300

    add main file

---
+++ src/main/java/com/dazavv/supply/supplymanagementservice/SupplyManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dazavv.supply.supplymanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyManagementServiceApplication.class, args);
+    }
+
+}
